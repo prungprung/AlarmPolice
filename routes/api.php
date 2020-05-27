926f486a964806bbc2b4d084a9991b07502aa5fa
Sirin <sirin.s@komgrip.com>
2020-05-27 17:21:27 +0700

    TestErrorGit

--- routes/api.php
+++ routes/api.php
@@ -17,8 +17,3 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::middleware('auth:api')->get('/sendrichmenu/{context}','RichMenu');
-Route::middleware('auth:api')->get('/sendriff','LiffLineController@Liff');
-Route::middleware('auth:api')->get('/sendtext','TextMessageLineController@TextMessageLine');
-Route::middleware('auth:api')->get('/sendflex','FlexLineController@Flex');
-Route::middleware('auth:api')->get('/sendnotify','NotifyController@Notify');
