0577f5304f5f5a91f57043e5bfd420872899e5bc
Sirin <sirin.s@komgrip.com>
2020-05-28 11:21:18 +0700

    liff

--- app/Http/Controllers/LiffLineController.php
+++ app/Http/Controllers/LiffLineController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class FlexLineController extends Controller
+class LiffLineController extends Controller
 {
 
     public function _construct()
