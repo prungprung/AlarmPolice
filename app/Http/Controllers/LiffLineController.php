f23fea13e90cd8c6598c9fcb315a4eddb1202c7e
Sirin <sirin.s@komgrip.com>
2020-05-28 11:42:48 +0700

    liff

--- app/Http/Controllers/LiffLineController.php
+++ app/Http/Controllers/LiffLineController.php
@@ -12,7 +12,7 @@ class LiffLineController extends Controller
 
     }
     public function Liff(Request $request){
-            $postbody = $request->json()->all();
+            $postbody = $request->json;
             print_r($request);
             print_r("\n");
             print_r($postbody);
