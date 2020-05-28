7376f9e876c2c970089df8233878f88861c1156b
Sirin <sirin.s@komgrip.com>
2020-05-28 12:02:33 +0700

    liff

--- app/Http/Controllers/LiffLineController.php
+++ app/Http/Controllers/LiffLineController.php
@@ -14,7 +14,6 @@ class LiffLineController extends Controller
     public function Liff(Request $request){
             $postbody = $request->data;
             print_r($postbody);
-            exit();
             return view('defaultview/showvalue',compact('postbody'));
     }
    
