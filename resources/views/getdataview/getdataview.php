530996fdab68d90f2ba4cef79b1f0d3dbdc86a00
Sirin <sirin.s@komgrip.com>
2020-05-28 10:33:10 +0700

    liff

--- resources/views/getdataview/getdataview.php
+++ resources/views/getdataview/getdataview.php
@@ -1,57 +1,61 @@
 <html>
+
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=0,viewport-fit=cover">
-  <title>My LIFF App</title>
-  <link rel="stylesheet" href="css/style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=0,viewport-fit=cover">
+    <title>My LIFF App</title>
 </head>
+
 <body>
-  <script src="https://static.line-scdn.net/liff/edge/versions/2.1.13/sdk.js"></script>
-  <script>
-
-    function logOut() {
-      liff.logout()
-      window.location.reload()
-    }
-
-    function closed() {
-      liff.closeWindow()
-    }
-    
-    function openWindow() {
-      liff.openWindow({
-        url: "https://line.me",
-        external: true
-      })
-    }
-
-     async function main() {
-      liff.ready.then(() => {
-        const profile = await liff.getProfile()
-        document.getElementById("isLoggedIn").append(liff.isLoggedIn())
-        var json = {"userId": profile.userId,
-                    "displayName":profile.displayName,
-                    "decodedIDToken":liff.getDecodedIDToken().email,
-                    "utouId":liff.getContext().utouId,
-                    "accessToken":liff.getAccessToken()
-                    }
-        if (liff.isLoggedIn()) {
-            $.ajax({
-            type: "POST",
-            url: "/senddata",
-            data: json,
-            dataType: "json",
-            success:function(data) {
-                 alert("success")
-        } 
-});
-        } else {
-          liff.login()
+    <script src="https://static.line-scdn.net/liff/edge/versions/2.1.13/sdk.js"></script>
+    <script>
+        function logOut() {
+            liff.logout()
+            window.location.reload()
         }
-      })
-      await liff.init({ liffId: "1654272826-Og8LnyZ6" })
-    }
-    main()
-  </script>
+
+        function closed() {
+            liff.closeWindow()
+        }
+
+        function openWindow() {
+            liff.openWindow({
+                url: "https://line.me",
+                external: true
+            })
+        }
+
+        async function main() {
+            liff.ready.then(() => {
+                const profile = await liff.getProfile()
+                document.getElementById("isLoggedIn").append(liff.isLoggedIn())
+                var json = {
+                    "userId": profile.userId,
+                    "displayName": profile.displayName,
+                    "decodedIDToken": liff.getDecodedIDToken().email,
+                    "utouId": liff.getContext().utouId,
+                    "accessToken": liff.getAccessToken()
+                }
+                if (liff.isLoggedIn()) {
+                    $.ajax({
+                        type: "POST",
+                        url: "/senddata",
+                        data: json,
+                        dataType: "json",
+                        success: function(data) {
+                            alert("success")
+                        }
+                    });
+                } else {
+                    liff.login()
+                }
+            })
+            await liff.init({
+                liffId: "1654272826-Og8LnyZ6"
+            })
+        }
+        main()
+    </script>
 </body>
+
 </html>
