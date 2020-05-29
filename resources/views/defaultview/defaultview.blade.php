0556ccc43c2e52de64bbb3d2228cf206d9601cb2
Sirin <sirin.s@komgrip.com>
2020-05-29 14:43:41 +0700

    show null

--- resources/views/defaultview/defaultview.blade.php
+++ resources/views/defaultview/defaultview.blade.php
@@ -1,4 +1,3 @@
-<button type="button" onclick="window.location='{{ url("/sendtext") }}'">sendText</button>
 <button type="button" onclick="window.location='{{ url("/sendflex") }}'">sendFlex</button>
 <button type="button" onclick="window.location='{{ url("/sendnotify") }}'">sendNotify</button>
 <button type="button" onclick="window.location='{{ url("/sendrichmenu/login") }}'">sendRichMenuLogin</button>
