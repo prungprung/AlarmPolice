8e46d85083bb67ea3d73c75d072b5ba38bee97db
Sirin <sirin.s@komgrip.com>
2020-05-29 13:54:09 +0700

    show null

--- resources/views/car.blade.php
+++ resources/views/car.blade.php
@@ -17,10 +17,9 @@
         width: 33%;
     }
 </style>
-
 <body>
-    <div>
-    </div>
+<img src="http://intense-scrubland-71413.herokuapp.com/public/image/linerichmenu_1_.jpeg" alt="Trulli" width="150" height="150">
+
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <script src="https://static.line-scdn.net/liff/edge/versions/2.1.13/sdk.js"></script>
     <script>
