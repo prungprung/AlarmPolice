4d2ef29802c1b07c4a798e6a40a40848e6811959
Sirin <sirin.s@komgrip.com>
2020-06-01 12:39:09 +0700

    My first commit

--- app/Http/Controllers/RichMenuController.php
+++ app/Http/Controllers/RichMenuController.php
@@ -153,7 +153,8 @@ class RichMenuController extends Controller
       ),
     ));
 if($context == "login"){
-  $urls = "https://api.line.me/v2/bot/user/".$this->getUserId()."/richmenu/".$richmenu;
+  $urls = "https://api.line.me/v2/bot/user/U4638e9a419fd8a40e2ee1164bda3145c/richmenu/".$richmenu;
+  // $urls = "https://api.line.me/v2/bot/user/".$this->getUserId()."/richmenu/".$richmenu;
 }else{
   $urls = "https://api.line.me/v2/bot/user/U4638e9a419fd8a40e2ee1164bda3145c/richmenu/".$richmenu;
 }
