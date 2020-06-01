6032b95c6ed297fd5aa9866a54c7b7395b4e2aa9
Sirin <sirin.s@komgrip.com>
2020-06-01 10:07:30 +0700

    commit

--- app/Http/Controllers/RichMenuController.php
+++ app/Http/Controllers/RichMenuController.php
@@ -168,7 +168,7 @@ exit();
     curl_close($curl);
     $curl = curl_init();
         curl_setopt_array($curl, array(
-      CURLOPT_URL => $url.$richmenu,
+      CURLOPT_URL => $url,
       CURLOPT_RETURNTRANSFER => true,
       CURLOPT_ENCODING => "",
       CURLOPT_MAXREDIRS => 10,
