56732cab8c650178b0c06c556aefea74094763eb
Sirin <sirin.s@komgrip.com>
2020-06-01 12:55:41 +0700

    My first commit

--- resources/views/login.php
+++ resources/views/login.php
@@ -37,7 +37,7 @@ user:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text"></
 function submits(){
     closed();
     // window.location.href = "https://liff.line.me/1654272826-Og8LnyZ6"
-    window.location.href ="/sendrichmenu/login/"
+    window.location.href ="/sendrichmenu/login"
     closed();
 }
         var userid;
