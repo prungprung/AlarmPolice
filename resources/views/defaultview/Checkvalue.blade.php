f89787950220e2244b41c9135e4814a31afea7b0
Sirin <sirin.s@komgrip.com>
2020-06-02 09:59:02 +0700

    My first commit

--- resources/views/defaultview/Checkvalue.blade.php
+++ resources/views/defaultview/Checkvalue.blade.php
@@ -1,4 +1,17 @@
-<?php
-dd($postbody)
-?>
-55555555555
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=0,viewport-fit=cover">
+    <title>Show value</title>
+</head>
+
+<body>
+<div>
+  <p>Taken from wikpedia</p>
+  <?php
+    dd($postbody);
+  ?>
+</div>
+</body>
+</html>
