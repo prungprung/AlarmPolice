371ef88865e1ea53c08fd70238440f05693139d7
Sirin <sirin.s@komgrip.com>
2020-06-04 15:06:55 +0700

    ChatBotFullfil25

--- app/Http/Controllers/ChatBotController.php
+++ app/Http/Controllers/ChatBotController.php
@@ -28,7 +28,6 @@ class ChatBotController extends Controller
           CURLOPT_ENCODING => "",
           CURLOPT_MAXREDIRS => 10,
           CURLOPT_TIMEOUT => 0,
-          CURLOPT_FOLLOWLOCATION => true,
           CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
           CURLOPT_CUSTOMREQUEST => "GET",
           CURLOPT_POSTFIELDS =>$data,
