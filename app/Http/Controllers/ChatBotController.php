ed0054047f0975e0b40aae5844c910d2c6207a6f
Sirin <sirin.s@komgrip.com>
2020-06-04 14:20:52 +0700

    ChatBotFullfil21

--- app/Http/Controllers/ChatBotController.php
+++ app/Http/Controllers/ChatBotController.php
@@ -11,32 +11,62 @@ class ChatBotController extends Controller
     }
     public function ChatBot()
     {
-        $method = $_SERVER['REQUEST_METHOD'];
-        if($method == "GET"){
-        $requestBody = file_get_contents('php://input');
-        $json = json_decode($requestBody);
-        $text = $json->queryResult->queryText;
-        
-        switch($text){
-            case 'hi':
-                $speech = "Hi my 8.";
-            break;
-            case '777':
-                $speech = "bye my 777.";
-            break;
-            default:
-            $speech = "Say default.";
-            break;
-        }
 
-        $response = new \stdClass();
-        $response->speech=gettype($text);
-        $response->displayText=$speech;
-        $response->source="webhook";
-        echo json_encode($response);
-        }else{
-        echo "metho not allowed";
-        }
+        $curl = curl_init();
+        $data = 
+        [
+          "queryResult"=> [
+            "queryText"=> "what",
+            "parameters"=> [],
+            "languageCode"=> "th"
+          ]
+        ];        
+        curl_setopt_array($curl, array(
+          CURLOPT_URL => "https://intense-scrubland-71413.herokuapp.com/public/sendchatbot",
+          CURLOPT_RETURNTRANSFER => true,
+          CURLOPT_ENCODING => "",
+          CURLOPT_MAXREDIRS => 10,
+          CURLOPT_TIMEOUT => 0,
+          CURLOPT_FOLLOWLOCATION => true,
+          CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+          CURLOPT_CUSTOMREQUEST => "GET",
+          CURLOPT_POSTFIELDS =>$data,
+          CURLOPT_HTTPHEADER => array(
+            "Content-Type: application/json",
+            "Cookie: XSRF-TOKEN=cfd65b096b3c4c2d87cf6ac4001d57d4"
+          ),
+        ));
+        
+        $response = curl_exec($curl);
+        
+        curl_close($curl);
+        echo $response;
+        // $method = $_SERVER['REQUEST_METHOD'];
+        // if($method == "GET"){
+        // $requestBody = file_get_contents('php://input');
+        // $json = json_decode($requestBody);
+        // $text = $json->queryResult->queryText;
+        
+        // switch($text){
+        //     case 'hi':
+        //         $speech = "Hi my 8.";
+        //     break;
+        //     case '777':
+        //         $speech = "bye my 777.";
+        //     break;
+        //     default:
+        //     $speech = "Say default.";
+        //     break;
+        // }
+
+        // $response = new \stdClass();
+        // $response->speech=gettype($text);
+        // $response->displayText=$speech;
+        // $response->source="webhook";
+        // echo json_encode($response);
+        // }else{
+        // echo "metho not allowed";
+        // }
         // return view('/defaultview/Checkvalue');
     }
   
