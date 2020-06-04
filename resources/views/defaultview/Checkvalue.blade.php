664e08de1a5f3e7fee72b75ab5cc41f4f1ad0ed1
Sirin <sirin.s@komgrip.com>
2020-06-04 11:13:53 +0700

    ChatBotFullfil6

--- resources/views/defaultview/Checkvalue.blade.php
+++ resources/views/defaultview/Checkvalue.blade.php
@@ -1,10 +1,3 @@
 <?php
-$method = $_SERVER['REQUEST_METHOD'];
-if($method == "POST"){
-  $requestBody = file_get_contents('php://input');
-  $json = json_decode($requestBody);
-  $text = $json->queryResult->queryText;
-}else{
-  echo "metho not allowed";
-}
+
 ?>
