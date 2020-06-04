71df97988c8975c0158704b6b2fe85ddd54bca2b
Sirin <sirin.s@komgrip.com>
2020-06-04 15:09:13 +0700

    ChatBotFullfil26

--- app/Http/Controllers/ChatBotController.php
+++ app/Http/Controllers/ChatBotController.php
@@ -13,30 +13,30 @@ class ChatBotController extends Controller
     {
 
         $curl = curl_init();
-        $data = 
-        [
-          "queryResult"=> [
-            "queryText"=> "what",
-            "parameters"=> [],
-            "languageCode"=> "th"
-          ]
-        ];
-        $urls ='https://api.line.me/v2/bot/message/push';
+        $data =
+            [
+                "queryResult" => [
+                    "queryText" => "what",
+                    "parameters" => ['55','55'],
+                    "languageCode" => "th"
+                ]
+            ];
+        $urls = 'https://api.line.me/v2/bot/message/push';
         curl_setopt_array($curl, array(
-          CURLOPT_URL => $urls,
-          CURLOPT_RETURNTRANSFER => true,
-          CURLOPT_ENCODING => "",
-          CURLOPT_MAXREDIRS => 10,
-          CURLOPT_TIMEOUT => 0,
-          CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-          CURLOPT_CUSTOMREQUEST => "GET",
-          CURLOPT_POSTFIELDS =>$data,
-          CURLOPT_HTTPHEADER => array(
-            "Content-Type: application/json",
-            "Cookie: XSRF-TOKEN=eyJpdiI6IncxVUZSNVIzVlRHNWxwMjZHRno1eWc9PSIsInZhbHVlIjoiU1l2NGt1bTIxWUNhM1BFT3VWVlRVa2tCQ1JDbElsNnpyVjZPQWRHUnh3LzhiR0tidUZzajZPYjZBUVlOdWZySCIsIm1hYyI6IjI2NWJkYWNlZTQ3MTc0YThkNjYxNjY0OTRkM2MzZmM5MzRjYzc0NmIzMmQ4OTI2NzQ4NDIzYTAxZDI1NTQzYWMifQ%3D%3D; laravel_session=eyJpdiI6IjdrVklEWjZLYlhCWFVyKzhoZUluUVE9PSIsInZhbHVlIjoiYUlobG5mYUpIQUhIMEFjT1hWUm40WURWYVlHaTYzcURSMDJTVlYwb3RLSW1OVzRZbS9EZVlsUEVLNnE3TG1lVyIsIm1hYyI6IjJhYmUwNTg3OWE2MTEwNmZmYWFlODNlZGIwODUxMjIwMTcwYzY3ZmYxOWZmZmExZTMxYmE2M2VkZmNlODIyNDUifQ%3D%3D"
-          ),
+            CURLOPT_URL => $urls,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "GET",
+            CURLOPT_POSTFIELDS => $data,
+            CURLOPT_HTTPHEADER => array(
+                "authorization: Bearer mCJv5+R/NzahU6FczR8quazO0HpzsjHUhj8ygOptTepkz4VLY7GJ25ZY/IbmT0zCliv4ryqsdstJkJ2XaAKleH10Oor5/RfLWvWpZ8G5Z85xlABpWumYnTsfMYToaiaiK9k5wBEHiyWpR+xATHtY/QdB04t89/1O/w1cDnyilFU=",
+                "content-type: application/json"
+            ),
         ));
-        
+
         $response = curl_exec($curl);
         curl_close($curl);
         echo $response;
@@ -46,7 +46,7 @@ class ChatBotController extends Controller
         // $requestBody = file_get_contents('php://input');
         // $json = json_decode($requestBody);
         // $text = $json->queryResult->queryText;
-        
+
         // switch($text){
         //     case 'hi':
         //         $speech = "Hi my 8.";
@@ -69,5 +69,4 @@ class ChatBotController extends Controller
         // }
         // return view('/defaultview/Checkvalue');
     }
-  
 }
