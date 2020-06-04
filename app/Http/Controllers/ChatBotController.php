b05162e13667383abb3579d77534062e72cd7554
Sirin <sirin.s@komgrip.com>
2020-06-04 13:26:23 +0700

    ChatBotFullfil16

--- app/Http/Controllers/ChatBotController.php
+++ app/Http/Controllers/ChatBotController.php
@@ -11,41 +11,13 @@ class ChatBotController extends Controller
     }
     public function ChatBot()
     {
-        $data = 
-        [
-          "responseId"=> "2b5c795e-5cac-4239-939d-d1af4d475131-b4a98e7e",
-          "queryResult"=> [
-            "queryText"=> "อะไรนะ",
-            "parameters"=> [],
-            "languageCode"=> "th"
-          ]
-        ];
-    
-        
-
-        $curl = curl_init();
-        curl_setopt_array($curl, array(
-          CURLOPT_URL => "https://api.line.me/v2/bot/richmenu",
-          CURLOPT_RETURNTRANSFER => true,
-          CURLOPT_ENCODING => "",
-          CURLOPT_MAXREDIRS => 10,
-          CURLOPT_TIMEOUT => 30,
-          CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-          CURLOPT_CUSTOMREQUEST => "POST",
-          CURLOPT_POSTFIELDS => json_encode($data),
-          CURLOPT_HTTPHEADER => array(
-            "authorization: Bearer mCJv5+R/NzahU6FczR8quazO0HpzsjHUhj8ygOptTepkz4VLY7GJ25ZY/IbmT0zCliv4ryqsdstJkJ2XaAKleH10Oor5/RfLWvWpZ8G5Z85xlABpWumYnTsfMYToaiaiK9k5wBEHiyWpR+xATHtY/QdB04t89/1O/w1cDnyilFU=",
-            "content-type: application/json"
-          ),
-        ));
-
         $method = $_SERVER['REQUEST_METHOD'];
-        if($method == "POST"){
-        $requestBody = file_get_contents('php://input');
-        $json = json_decode($requestBody);
-        $text = $json->queryResult->queryText;
+        if ($method == "GET") {
+            $requestBody = file_get_contents('php://input');
+            $json = json_decode($requestBody);
+            $text = $json->queryResult->queryText;
         
-        switch($text){
+            switch ($text) {
             case 'hi':
                 $speech = "Hi my 8.";
             break;
@@ -56,17 +28,7 @@ class ChatBotController extends Controller
             $speech = "Say default.";
             break;
         }
-
-        $response = new \stdClass();
-        $response->speech=gettype($text);
-        $response->displayText=$speech;
-        $response->source="webhook";
-        echo json_encode($response);
-        return  $response->header('Content-Type', 'text/plain');
-        }else{
-        echo "metho not allowed";
+            // return view('/defaultview/Checkvalue');
         }
-        // return view('/defaultview/Checkvalue');
     }
-  
 }
