14afa8a17502cdd526513a264a3412acdcfb2f28
Sirin <sirin.s@komgrip.com>
2020-06-05 15:50:06 +0700

    database6

--- app/Http/Controllers/SendTextController.php
+++ app/Http/Controllers/SendTextController.php
@@ -36,7 +36,7 @@ class SendTextController extends Controller
                 CURLOPT_TIMEOUT => 0,
                 CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
                 CURLOPT_CUSTOMREQUEST => "POST",
-                CURLOPT_POSTFIELDS => json_encode($data),
+                CURLOPT_POSTFIELDS => $data,
                 CURLOPT_HTTPHEADER => array(
                     "authorization: Bearer mCJv5+R/NzahU6FczR8quazO0HpzsjHUhj8ygOptTepkz4VLY7GJ25ZY/IbmT0zCliv4ryqsdstJkJ2XaAKleH10Oor5/RfLWvWpZ8G5Z85xlABpWumYnTsfMYToaiaiK9k5wBEHiyWpR+xATHtY/QdB04t89/1O/w1cDnyilFU=",
                     "content-type: application/json"
