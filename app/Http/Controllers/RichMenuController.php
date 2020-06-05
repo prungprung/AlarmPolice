e9d26b9be85e231094b47a1bc52c8e2f83a7dd4b
Sirin <sirin.s@komgrip.com>
2020-06-05 12:59:01 +0700

    database2

--- app/Http/Controllers/RichMenuController.php
+++ app/Http/Controllers/RichMenuController.php
@@ -13,9 +13,6 @@ class RichMenuController extends Controller
   public function RichMenu(Request $request)
   {
     $postbody = $request->data;
-    $results = DB::select('select * from user');
-    print_r($results);
-    exit();
     if ($postbody['status'] == "login") {
       DB::insert('insert into user (id, line_id,display_name,police_id) values (?,?,?,?)', [1, $postbody['userId'],'888','999']);
       $dataFile = 'http://intense-scrubland-71413.herokuapp.com/public/image/linerichmenu_3_.jpeg';
