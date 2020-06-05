74a0aac0fa103a7f625db5119173eafb2ae21466
Sirin <sirin.s@komgrip.com>
2020-06-05 15:52:28 +0700

    database6

--- app/Http/Controllers/SendTextController.php
+++ app/Http/Controllers/SendTextController.php
@@ -27,6 +27,8 @@ class SendTextController extends Controller
                     ]
                 ]
                     ];
+                    echo json_encode($data);
+
             $urls = 'https://api.line.me/v2/bot/message/push';
             curl_setopt_array($curl, array(
                 CURLOPT_URL => $urls,
@@ -35,13 +37,14 @@ class SendTextController extends Controller
                 CURLOPT_MAXREDIRS => 10,
                 CURLOPT_TIMEOUT => 0,
                 CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                CURLOPT_CUSTOMREQUEST => "POST",
-                CURLOPT_POSTFIELDS => $data,
+                CURLOPT_CUSTOMREQUEST => "GET",
+                CURLOPT_POSTFIELDS => json_encode($data),
                 CURLOPT_HTTPHEADER => array(
                     "authorization: Bearer mCJv5+R/NzahU6FczR8quazO0HpzsjHUhj8ygOptTepkz4VLY7GJ25ZY/IbmT0zCliv4ryqsdstJkJ2XaAKleH10Oor5/RfLWvWpZ8G5Z85xlABpWumYnTsfMYToaiaiK9k5wBEHiyWpR+xATHtY/QdB04t89/1O/w1cDnyilFU=",
                     "content-type: application/json"
                 ),
             ));
+            exit();
         }
     }
     public function readText()
